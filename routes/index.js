90270a9776f0a29ac9c8a24d204a7030d40e79f4
Lostr <dima.gasii@yandex.ru>
2018-05-27 21:17:55 +0300

    Add module express-session

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/trash', function(req, res, next) {
-  console.log("Первая функция");
+  res.cookie('login', 'Dimas', {maxAge: 100000});
+  console.log(req.session)
+  console.log("Куки установлены");
   next();
 }, function(req, res, next){
   console.log("Вторая функция");
