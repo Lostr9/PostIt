ea9976b4bf9174429e2b300e76d46f8c0f4d76c9
Lostr <dima.gasii@yandex.ru>
2018-05-28 05:16:13 +0300

    Update model user

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var LocalStrategy = require('passport-local').Strategy;
+var passport = require('passport');
+var mongoose = require('mongoose');
 var conf = require('./config'); // config
 
 
@@ -20,6 +22,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('lostr'));
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+// mongoose
+mongoose.connect('mongodb://localhost/PostItNew');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
