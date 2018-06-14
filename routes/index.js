8fca727f4c2364a5c12de093db38918e4de2d6f5
Lostr <dima.gasii@yandex.ru>
2018-06-14 04:07:37 +0300

    Сompleted main page

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,52 @@ var User = require('../models/user');
 router.get('/', function(req,res){
   if (req.user){
     res.redirect('/profile');
-    res.render('index', {info: req.user});
   }else{
-    res.render('authPage', {loginError: '', regError: ''});
+    res.render('index', {LoginError: '', RegisterError: ''});
   }
+});
+
+router.post('/login', function(req, res) {
+  console.log(req.body);
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { return res.render('index', {LoginError: 'Неверное имя пользователя или пароль', RegisterError: ''});}//res.render('authPage', {loginError: 'Неверное имя пользователя или пароль', regError: ''}); }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/');
+    });
+  })(req, res);
+});
+
+router.post('/registration', function(req, res) {
+  if (req.body.password == req.body.password1){
+    User.register(new User({ username : req.body.username, email: req.body.email }), req.body.password, function(err, user) {
+      if (err) {
+        return res.render('index', {LoginError: '', RegisterError: err});//res.render('authPage', {loginError: '', regError: err});
+      }
+      console.log('Зареган');
+      passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+        if (!user) { return res.render('index', {LoginError: 'Неверное имя пользователя или пароль', RegisterError: ''});}//res.render('authPage', {loginError: 'Неверное имя пользователя или пароль', regError: ''}); }
+        req.logIn(user, function(err) {
+          if (err) { return next(err); }
+          return res.redirect('/');
+        });
+      })(req, res);
+    });
+  }
+  else{
+    res.render('index', {LoginError: '', RegisterError: 'Пароли не совпадают'});
+  }
+});
+
+router.get('/profile', function(req, res) {
+  res.send("Вы вошли");
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 
