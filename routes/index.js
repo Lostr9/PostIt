be4184e707356fdbda8e5b3942a172090a08dbb4
Lostr <dima.gasii@yandex.ru>
2018-06-19 15:49:18 +0300

    Push for host

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('../auth');
 var mongoose = require('mongoose');
+var urlParser = require('url');
 var User = require('../models/user');
 
 router.get('/', function(req,res){
@@ -23,25 +24,24 @@ router.post('/login', function(req, res) {
   })(req, res);
 });
 
+router.get('/login', function(req, res) {
+  res.redirect('/');
+});
+
 router.post('/registration', function(req, res) {
   if (req.body.password == req.body.password1){
     User.register(new User({ username : req.body.username, email: req.body.email }), req.body.password, function(err, user) {
       if (err) {
-<<<<<<< HEAD
-        return res.render('index', {LoginError: '', RegisterError: err});//res.render('authPage', {loginError: '', regError: err});
-      }
-      console.log('Зареган');
-=======
         if ('MongoError' == err.name){
           return res.render('index', {LoginError: '', RegisterError: 'Email уже используется'});
         }else if ('UserExistsError' == err.name){
           return res.render('index', {LoginError: '', RegisterError: 'Имя занято'});
         }else if ('MissingUsernameError' == err.name){
           return res.render('index', {LoginError: '', RegisterError: 'Заполните все поля'});
+        }else {
+          return res.render('index', {LoginError: '', RegisterError: 'Ошибка регистрации'});
         }
-        //res.render('authPage', {loginError: '', regError: err});
       }
->>>>>>> 357c3619e3f956719bf82bb0e3fc9c305542535c
       passport.authenticate('local', function(err, user, info) {
         if (err) { return next(err); }
         if (!user) { return res.render('index', {LoginError: 'Неверное имя пользователя или пароль', RegisterError: ''});}//res.render('authPage', {loginError: 'Неверное имя пользователя или пароль', regError: ''}); }
@@ -57,8 +57,16 @@ router.post('/registration', function(req, res) {
   }
 });
 
+router.get('/registration', function(req, res) {
+  res.redirect('/');
+});
+
 router.get('/profile', function(req, res) {
-  res.render('profile');
+  if(req.user){
+    res.render('profile', {Username: req.user.username, Email: req.user.email, Post: req.user.posts, SocialNetworks: req.user.networks.length});
+  }else{
+    res.redirect('/');
+  }
 });
 
 router.get('/logout', function(req, res) {
@@ -66,5 +74,30 @@ router.get('/logout', function(req, res) {
   res.redirect('/');
 });
 
+router.get('/history', function(req, res) {
+  if(req.user){
+    res.render('history', {posts: ''});
+  }else{
+    res.redirect('/');
+  }
+});
+
+router.get('/networks', function(req, res) {
+  //myurls = new url.URL ('https://oauth.vk.com/blank.html#access_token=19d113a81c0f853835288c8ec054a250c5c01e86d32559456bd49f75bbd62f61b975c54b73640931be7ee&expires_in=0&user_id=61486425&state=123456');
+  //console.log(myurls.searchParams.get('access_token'));
+  url = urlParser.parse('https://oauth.vk.com/blank.html#access_token=19d113a81c0f853835288c8ec054a250c5c01e86d32559456bd49f75bbd62f61b975c54b73640931be7ee&expires_in=0&user_id=61486425&state=123456');
+
+  //console.log(url.searchParams('access_token'));
+  tmp = url.hash.split('&');
+  console.log(tmp);
+  token = tmp[0].split("=");
+  id_user = tmp[2].split("=");
+  console.log('Переход');
+  res.send(id_user);
+});
+
+router.post('/networks', function(req, res) {
+  console.log(req);
+});
 
 module.exports = router;
